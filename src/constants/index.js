e5e07d70e20451334d57afd344f9e39f0587aa9d
Agrim Gupta <agrimgupt195@gmail.com>
2025-08-16 00:38:13 +0530

    add portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,218 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "AI Fitness Agent",
+    description:
+      "Developed a voice-based AI fitness assistant that creates personalized diet and workout plans tailored to individual goals and preferences.",
+    subDescription: [
+      "Developed a Next.js application with seamless Vapi AI voice integration for real-time AI conversations.",
+      "Implemented Clerk authentication to provide secure and scalable user sign-in and account management.",
+      "Utilized ConvexDB for real-time, serverless database operations and optimized data querying.",
+      "Ensured performance optimization and scalability for handling concurrent voice interactions.",
+    ],
+    href: "https://github.com/AgrimGupta195/AI-FITNESS-ASSISTANT",
+    logo: "/assets/logos/hero-ai.png",
+    image: "/assets/logos/image.png",
+    tags: [
+      {
+        id: 1,
+        name: "Next.js",
+        path: "/assets/logos/nextjs-icon-svgrepo-com.svg",
+      },
+      {
+        id: 2,
+        name: "Vapi AI",
+        path: "/assets/logos/vapi.jpg",
+      },
+      {
+        id: 3,
+        name: "ConvexDB",
+        path: "/assets/logos/convex.png",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Chat Application",
+    description:
+      "Developed a real-time chat application using WebSockets to enable instant, bidirectional communication.",
+    subDescription: [
+      "Built a real-time chat application leveraging WebSockets for low-latency, bidirectional communication.",
+      "Implemented user authentication using JWT and bcrypt.js for secure login and password management.",
+      "Developed a React-based frontend with Tailwind CSS for a sleek user experience.",
+      "Connected to a secure MongoDB database for user data storage.",
+    ],
+    href: "https://github.com/AgrimGupta195/fullstack-chat-app",
+    logo: "",
+    image: "/assets/logos/chatty.png",
+    tags: [
+      {
+        id: 1,
+        name: "WebSockets",
+        path: "/assets/logos/Websockets.png",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.jpeg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Linkedin Clone",
+    description:
+      "A modern, interactive web application built with React , Node.js, and MongoDB, mimicking the core features of LinkedIn.",
+    subDescription: [
+      "Implemented real-time updates for likes, comments, and connections to enhance user engagement.",
+      "Implemented API interactions using Node.js (Express.js) for a robust backend.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UX.",
+      "Integrated MongoDB for efficient client-side database storage.",
+    ],
+    href: "https://github.com/AgrimGupta195/unlikedin",
+    logo: "",
+    image: "/assets/logos/linkedin.png",
+    tags: [
+       {
+        id: 1,
+        name: "Nodejs",
+        path: "/assets/logos/nodejs.jpg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "MongoDB",
+        path: "/assets/logos/mongo.jpeg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "E-Commerce Platform",
+    description:
+      "A full-stack e-commerce platform with user authentication, product management, and payment integration.",
+    subDescription: [
+      "Integrated order tracking, real-time cart updates, and automatic email notifications, reducing admin workload and improving customer satisfaction.",
+      "Used Vite, Tailwind CSS, and React Router to create a fast, seamless, and responsive user interface with real-time interactions.",
+      "Implemented secure payment processing using Stripe API, ensuring safe transactions.",
+      "Implemented user authentication and authorization using JWT, ensuring secure access to sensitive data.",
+    ],
+    href: "https://github.com/AgrimGupta195/XUXU-Ecommerce",
+    logo: "",
+    image: "/assets/logos/ecommerce.png",
+    tags: [
+       {
+        id: 1,
+        name: "Nodejs",
+        path: "/assets/logos/nodejs.jpg",
+      },
+      {
+        id: 2,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 3,
+        name: "Stripe",
+        path: "/assets/logos/stripe-v2.svg",
+      },
+      {
+        id: 4,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Flappy Bird Game",
+    description:
+      "Developed a Flappy Bird clone using Vanilla JavaScript, HTML5, and CSS3, featuring responsive design and smooth animations.",
+    subDescription: [
+      "Created a fully functional Flappy Bird clone with responsive design and smooth animations using Vanilla JavaScript, HTML5, and CSS3.",
+      "Implemented game mechanics such as collision detection, scoring, and level progression.",
+      "Optimized performance for smooth gameplay across various devices."
+    ],
+    href: "https://github.com/AgrimGupta195/FlappyBird",
+    logo: "",
+    image: "/assets/logos/flappy.png",
+    tags: [
+      {
+        id: 1,
+        name: "HTML5",
+        path: "/assets/logos/html5.svg",
+      },
+      {
+        id: 2,
+        name: "CSS3",
+        path: "/assets/logos/css3.svg",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        path: "/assets/logos/javascript.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "Linkedin",
+    href: "https://www.linkedin.com/in/agrim-gupta-772880351/",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/agrim___gupta/",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Web Developer",
+    job: "Cloud Computing Cell",
+    date: "2024-2025",
+    contents: [
+      "Designed and optimized backend services using Node.js, MongoDB, and MySQL, improving system efficiency",
+      "Led a team to develop an exam portal for 450+ real-time users, ensuring high availability and scalability.",
+      "Debugged and troubleshot backend issues using Linux/Unix command-line tools."
+    ],
+  },
+  {
+    title: "Full Stack Developer Intern",
+    job: "Digital Guruji by HireCream",
+    date: "2025-Present",
+    contents: [
+      "Integrated payment gateways (Stripe) for seamless online transactions",
+      "Created responsive web pages using React.js for seamless user Experience.Used Zustand to manage states.",
+      "Worked on backend debugging and performance tuning, resolving critical system issues.",
+      "Resolved bugs in User Authentication System."
+    ],
+  },
+];
