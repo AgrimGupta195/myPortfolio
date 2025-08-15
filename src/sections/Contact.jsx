a346877b985e30c9ff84cecd5f56a670e8724855
Agrim Gupta <128774219+AgrimGupta195@users.noreply.github.com>
2025-08-16 00:52:05 +0530

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -30,13 +30,13 @@ const Contact = () => {
     try {
       console.log("From submitted:", formData);
       await emailjs.send(
-        "service_79b0nyj",
-        "template_17us8im",
+        "service_5vtlk4r",
+        "template_5n6mgv8",
         {
           from_name: formData.name,
-          to_name: "Ali",
+          to_name: "Agrim Gupta",
           from_email: formData.email,
-          to_email: "AliSanatiDev@gmail.com",
+          to_email: "iamagrimgupta@outlook.com",
           message: formData.message,
         },
         "pn-Bw_mS1_QQdofuV"
